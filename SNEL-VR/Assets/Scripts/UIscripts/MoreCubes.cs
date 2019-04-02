eed7fca88f9ad3bc46d1a515f9e4002ac2a93398
alzimdv <alesundv@student.chalmers.se>
2019-04-02 16:59:56 +0200

    Gjorde en knapp som spawnar kuber när den trycks

---
+++ SNEL-VR/Assets/Scripts/UIscripts/MoreCubes.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoreCubes : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    public Vector3 spawn = new Vector3(15, 5, 0);
+
+    public void Cube()
+    {
+        Instantiate(cubePrefab, spawn, Quaternion.identity);
+    }
+}
