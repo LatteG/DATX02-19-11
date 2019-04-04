716f7e3820d77d23513f8ce2b1f5d6c603e473fc
Alexander Sundvall <alesundv@net.chalmers.se>
2019-04-04 15:45:37 +0200

    Added a interactable cube

---
+++ SNEL-VR/Assets/Scripts/UIscripts/TeleportCube.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportCube : MonoBehaviour
+{
+    public void Teleport()
+    {
+        Vector3 pos = transform.position;
+        pos.y += 1;
+        transform.position = pos;
+    }
+}
