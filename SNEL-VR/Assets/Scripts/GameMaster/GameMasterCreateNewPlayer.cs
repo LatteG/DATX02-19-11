5bf06f9514afa808e9c6c75cbc8aa4ca21c3ad83
guseismjo <guseismjo@student.gu.se>
2019-04-08 19:01:29 +0200

    Added spawn points for players and supporting scripts

---
+++ SNEL-VR/Assets/Scripts/GameMaster/GameMasterCreateNewPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameMasterCreateNewPlayer : MonoBehaviour
+{
+    private GameMasterManager gmm;
+    public GameObject player;
+
+    public void Start()
+    {
+        gmm = transform.parent.GetComponent<GameMasterManager>();
+        SpawnPlayer();
+    }
+    public void SpawnPlayer()
+    {
+        foreach(MeshCollider mc in gmm.GetAvailableSpawns())
+        {
+            if (!gmm.IsBusy(mc.GetHashCode()))
+            {
+                Vector3 pos = new Vector3(mc.bounds.max.x, mc.bounds.max.y, mc.bounds.max.z);
+                GameObject newPlayer = Instantiate(player, pos, Quaternion.identity, mc.transform);
+                gmm.UpdateSpawnBusy(mc.GetHashCode(), true);
+                break;
+            }
+        }
+    }
+}
---
+++ SNEL-VR/Assets/Scripts/GameMaster/GameMasterManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMasterManager : MonoBehaviour
+{
+    //Hash map for spawns. Might be able to use only list instead, and add/remove from it
+    private Dictionary<float, bool> spawnBusy;
+
+    private List<MeshCollider> availableSpawns;
+
+    private Transform playerSpawner;
+
+    public void Start()
+    {
+        playerSpawner = transform.GetChild(0);
+        InitSpawns();
+    }
+
+    //Search for MeshColliders and add them to hash map
+    private void InitSpawns()
+    {
+        spawnBusy = new Dictionary<float, bool>();
+        availableSpawns = new List<MeshCollider>();
+
+        foreach (Transform child in playerSpawner)
+        {
+            spawnBusy.Add(child.GetComponent<MeshCollider>().GetHashCode(), false);
+            availableSpawns.Add(child.GetComponent<MeshCollider>());
+        }
+
+        //Tmp
+        Debug.Log(spawnBusy.Count);
+    }
+
+    //Checks if spot is free or not
+    public bool IsBusy(float mc)
+    {
+        return spawnBusy[mc];
+    }
+
+    //In case we want to be able to add spawn points in game
+    public void AddRemoveSpawnBusy(float mc, bool add)
+    {
+        if (add) spawnBusy.Add(mc, false);
+        else spawnBusy.Remove(mc);
+    }
+
+    public void UpdateSpawnBusy(float mc, bool status)
+    {
+        spawnBusy[mc] =  status;
+    }
+
+    public Dictionary<float, bool> GetSpawnBusy()
+    {
+        return this.spawnBusy;
+    }
+
+    public List<MeshCollider> GetAvailableSpawns()
+    {
+        return this.availableSpawns;
+    }
+}
