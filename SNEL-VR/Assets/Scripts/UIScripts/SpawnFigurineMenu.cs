89e50f046547d36b59b8cb3053ab6076b1e1b3a8
alzimdv <alesundv@student.chalmers.se>
2019-04-09 18:32:46 +0200

    Clicking on player figurine spawns a menu with buttons

---
+++ SNEL-VR/Assets/Scripts/UIScripts/SpawnFigurineMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFigurineMenu : MonoBehaviour
+{
+    public Canvas figurineMenu;
+    private bool menuEnabled = false;
+
+    
+
+    public void spawnMenu()
+    {
+        if(!menuEnabled)
+        {
+            Instantiate(figurineMenu);
+            Camera MyCamera = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+            GameObject Mypointer = GameObject.FindWithTag("LaserPointer").GetComponent<GameObject>();
+            OVRRaycaster laser = figurineMenu.GetComponent<OVRRaycaster>().GetComponentInChildren<OVRRaycaster>();
+            figurineMenu.worldCamera = MyCamera;
+            laser.pointer = Mypointer;
+            menuEnabled = true;
+        }
+        else
+        {
+            Destroy(figurineMenu);
+            menuEnabled = false;
+        }
+
+        
+    }
+}
---
+++ SNEL-VR/Assets/Scripts/UIScripts/SpawnText.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpawnText : MonoBehaviour
+{
+    public Text text;
+
+    public void Text()
+    {
+        Instantiate(text);
+    }
+}
