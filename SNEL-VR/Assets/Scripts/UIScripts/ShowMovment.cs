cbf869d8037c595a32c1af680802399e0f54c177
alzimdv <alesundv@student.chalmers.se>
2019-04-18 13:27:13 +0200

    Made a script that highlights the grid squares in movement range.

---
+++ SNEL-VR/Assets/Scripts/UIScripts/ShowMovment.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ShowMovment : MonoBehaviour
+{
+    private bool highlighted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        highlighted = false;
+    }
+
+    // Finds and highlights the the squares within movement range
+    public void Highlight()
+    {
+        GridHandler gridhandler = new GridHandler();
+        GameObject figurine = GameObject.FindWithTag("PlayerFigurine");
+        HashSet<GameObject> movement = gridhandler.FindSquares(figurine);
+        foreach(GameObject square in movement)
+        {
+            Renderer renderer = square.GetComponent<Renderer>();
+            renderer.material.SetColor("_Color", Color.cyan);
+        }
+    }
+}
