4249582ec082567f60a34ee1abed3f510c28e452
alzimdv <alesundv@student.chalmers.se>
2019-04-25 17:33:12 +0200

    Highlighting can be toggled on and off.

--- SNEL-VR/Assets/Scripts/UIScripts/ShowMovment.cs
+++ SNEL-VR/Assets/Scripts/UIScripts/ShowMovment.cs
@@ -6,23 +6,38 @@ using UnityEngine;
 public class ShowMovment : MonoBehaviour
 {
     private bool highlighted;
+    private HashSet<GameObject> movement;
 
     // Start is called before the first frame update
     void Start()
     {
         highlighted = false;
+        movement = new HashSet<GameObject>();
     }
 
     // Finds and highlights the the squares within movement range
     public void Highlight()
     {
-        GridHandler gridhandler = GameObject.FindWithTag("GridHandler").GetComponent<GridHandler>();
-        GameObject figurine = GameObject.FindWithTag("PlayerFigurine");
-        HashSet<GameObject> movement = gridhandler.FindSquares(figurine);
-        foreach(GameObject square in movement)
+        if (!highlighted)
         {
-            Renderer renderer = square.GetComponent<Renderer>();
-            renderer.material.SetColor("_Color", new Color(0, 0xba, 0xbe));
+            GridHandler gridhandler = GameObject.FindWithTag("GridHandler").GetComponent<GridHandler>();
+            GameObject figurine = GameObject.FindWithTag("PlayerFigurine");
+            movement = gridhandler.FindSquares(figurine);
+            foreach (GameObject square in movement)
+            {
+                Renderer renderer = square.GetComponent<Renderer>();
+                renderer.material.SetColor("_Color", new Color(0, 0xba, 0xbe));
+            }
+            highlighted = true;
+        }
+        else
+        {
+            foreach (GameObject square in movement)
+            {
+                Renderer renderer = square.GetComponent<Renderer>();
+                renderer.material.SetColor("_Color", Color.white);
+            }
+            highlighted = false;
         }
     }
 }
