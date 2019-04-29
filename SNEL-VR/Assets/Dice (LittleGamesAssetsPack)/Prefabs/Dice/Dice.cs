08f15d76112753502f4e1cfb683425bdc4068d86
Dajly <jim_nordstrom_92@hotmail.com>
2019-04-30 00:16:14 +0200

    Dice

---
+++ SNEL-VR/Assets/Dice (LittleGamesAssetsPack)/Prefabs/Dice/Dice.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dice : MonoBehaviour
+{
+    Rigidbody rb;
+
+    bool hasLanded;
+    bool thrown;
+
+    Vector3 initPosition;
+
+    public int diceValue;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        initPosition = transform.position;
+
+    }
+}
