ce98df38d5983fa28fc40246ef7527af7cf2ad0f
alzimdv <alesundv@student.chalmers.se>
2019-05-04 15:35:43 +0200

    Trying to make an object follow the player at a set distance.

---
+++ SNEL-VR/Assets/Scripts/UIScripts/Dragging.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Dragging : MonoBehaviour
+{
+    public GameObject player;
+
+    private bool enabledDrag;
+    private Vector3 pos;
+
+    private Vector3 posOffset = new Vector3(1, 0, 0);
+    private float floatDistance = 1.0f;
+
+    public void func()
+    {
+        enabledDrag = !enabledDrag;
+    }
+
+    private void Start()
+    {
+        pos = transform.position;
+        enabledDrag = false;
+    }
+
+    public void Update()
+    {
+        pos = player.transform.position + posOffset;
+    }
+}
