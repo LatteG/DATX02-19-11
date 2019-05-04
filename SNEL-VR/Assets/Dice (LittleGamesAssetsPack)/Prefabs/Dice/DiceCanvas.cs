728f0641755ad2d50823b20ae844a2a2f360af88
dajly <jimn@student.chalmers.se>
2019-05-04 20:52:10 +0200

    Dice functions
    Added function for button B, it opens a dice menu on top of left hand.
    Added a temporary dice table with a container for gathering dice.
    Right now deactivating menu will also spawn a new dice on the dice table.

---
+++ SNEL-VR/Assets/Dice (LittleGamesAssetsPack)/Prefabs/Dice/DiceCanvas.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceCanvas : MonoBehaviour
+{
+    public GameObject diceMenu;
+    public GameObject cup;
+    public GameObject dice;
+
+    private Vector3 pos = new Vector3(16.5f,2,3.3f);
+
+    private bool enabledMenu = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.Two) && !enabledMenu)
+        {
+            diceMenu.SetActive(true);
+            enabledMenu = true;
+            Instantiate(dice, pos, Quaternion.identity);
+        }
+        else if (OVRInput.GetDown(OVRInput.Button.Two) && enabledMenu)
+        {
+            diceMenu.SetActive(false);
+            enabledMenu = false;
+        }
+    }
+}
