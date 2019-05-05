1e67e6f0c1c21048427fbec8a97d7d5171fb1778
alzimdv <alesundv@student.chalmers.se>
2019-05-05 15:34:27 +0200

    Canvas can be clicked, moved with the other hand, and when clicked again it remains in place. It always faces the player.

--- SNEL-VR/Assets/Scripts/UIScripts/Dragging.cs
+++ SNEL-VR/Assets/Scripts/UIScripts/Dragging.cs
@@ -4,25 +4,42 @@ using UnityEngine;
 
 public class Dragging : MonoBehaviour
 {
+    public GameObject anchor;
+
     Vector3 playerPos;
     Vector3 myPos;
-    int count = 0;
+    Vector3 unitPosOffest;
+
+    bool enabledDragging;
 
     private void Start()
     {
-        myPos = transform.position;
+        myPos = transform.parent.position;
+        unitPosOffest = Vector3.forward;
+        enabledDragging = false;
     }
 
     private void Update()
     {
-        playerPos = GameObject.FindObjectOfType<OVRPlayerController>().transform.position;
-        transform.position = playerPos + Vector3.forward * 2;
-        count++;
-        if(count > 100)
+
+        if (enabledDragging)
         {
-            Debug.Log(playerPos);
-            count = 0;
+            unitPosOffest = anchor.transform.forward;
+
+            playerPos = GameObject.FindObjectOfType<OVRPlayerController>().transform.position;
+            transform.parent.position = new Vector3(playerPos.x, playerPos.y + 0.5f, playerPos.z) + unitPosOffest * 0.5f;
         }
+        else
+        {
+            playerPos = GameObject.FindObjectOfType<OVRPlayerController>().transform.position;
+            transform.parent.position = new Vector3(playerPos.x, playerPos.y + 0.5f, playerPos.z) + unitPosOffest * 0.5f;
+        }
+    }
+
+    public void Drag()
+    {
+        enabledDragging = !enabledDragging;
+        Debug.Log("pointer down");
     }
 }
 
