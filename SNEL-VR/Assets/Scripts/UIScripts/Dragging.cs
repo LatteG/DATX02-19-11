99fd46f40b5cd79e239f3ebdfb1bfc051e60bd31
dajly <jimn@student.chalmers.se>
2019-05-05 11:44:36 +0200

    Canvas moves with the player without rotating around with the camera.

--- SNEL-VR/Assets/Scripts/UIScripts/Dragging.cs
+++ SNEL-VR/Assets/Scripts/UIScripts/Dragging.cs
@@ -1,31 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class Dragging : MonoBehaviour
 {
-    public GameObject player;
-
-    private bool enabledDrag;
-    private Vector3 pos;
-
-    private Vector3 posOffset = new Vector3(1, 0, 0);
-    private float floatDistance = 1.0f;
-
-    public void func()
-    {
-        enabledDrag = !enabledDrag;
-    }
+    Vector3 playerPos;
+    Vector3 myPos;
+    int count = 0;
 
     private void Start()
     {
-        pos = transform.position;
-        enabledDrag = false;
+        myPos = transform.position;
     }
 
-    public void Update()
+    private void Update()
     {
-        pos = player.transform.position + posOffset;
+        playerPos = GameObject.FindObjectOfType<OVRPlayerController>().transform.position;
+        transform.position = playerPos + Vector3.forward * 2;
+        count++;
+        if(count > 100)
+        {
+            Debug.Log(playerPos);
+            count = 0;
+        }
     }
 }
+
