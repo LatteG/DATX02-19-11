2093a8287db3defa34810b795c17b4a8963be639
LatteG <lahtig@student.chalmers.se>
2019-05-06 15:44:31 +0200

    Solved issue where dice could not be removed if the dice menu was closed and then opened again. This was done by enabling or disabling the canvas element in canvas instead of the entire game object.

--- SNEL-VR/Assets/Dice (LittleGamesAssetsPack)/Prefabs/Dice/DiceCanvas.cs
+++ SNEL-VR/Assets/Dice (LittleGamesAssetsPack)/Prefabs/Dice/DiceCanvas.cs
@@ -5,20 +5,19 @@ using UnityEngine;
 public class DiceCanvas : MonoBehaviour
 {
     public GameObject diceMenu;
-    //public GameObject cup;
-    //public GameObject dice;
-    //public Transform Spawn;
+    private Canvas canvas;
 
-
-    private bool enabledMenu = false;
+    private void OnEnable()
+    {
+        canvas = diceMenu.GetComponent<Canvas>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (OVRInput.GetDown(OVRInput.Button.Two))
         {
-            enabledMenu = !enabledMenu;
-            diceMenu.SetActive(enabledMenu);
+            canvas.enabled = !canvas.enabled;
         }
     }
 }
